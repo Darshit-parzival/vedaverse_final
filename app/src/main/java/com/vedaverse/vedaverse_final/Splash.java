7e0957bfdfcce8395c0856dbdf4baf112089df01
Darshit Rakhasiya <71558362+Darshit-parzival@users.noreply.github.com>
2024-05-19 14:11:43 +0530

    01

--- app/src/main/java/com/vedaverse/vedaverse_final/Splash.java
+++ app/src/main/java/com/vedaverse/vedaverse_final/Splash.java
@@ -1,14 +1,23 @@
 package com.vedaverse.vedaverse_final;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        new Handler().postDelayed(() -> {
+            Intent loginIntent = new Intent(Splash.this, Login.class);
+            startActivity(loginIntent);
+            overridePendingTransition(R.anim.slide_up, R.anim.fade_out);
+            finish();
+        }, SPLASH_TIME_OUT);
     }
 }
